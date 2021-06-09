f606436fd185aec9e133b36e635cfa0c3f790f66
Darwin Arevalo <dalexis.da@gmail.com>
2021-06-08 23:33:55 -0600

    Health check endpoint

--- BackEnd/authentication/src/routes/index.ts
+++ BackEnd/authentication/src/routes/index.ts
@@ -1,10 +1,11 @@
-import { Router } from "express";
+import { Router, Application } from "express";
 import {
     login,
     register
 } from "../controllers";
 const router = Router();
 
+router.get('/', (req, res) => { res.send(`${req.app.get('servicename')} service working!`) })
 router.post('/login', login);
 router.post('/register', register);
 
