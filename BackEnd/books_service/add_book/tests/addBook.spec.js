ef7933d36bc250570f71b7088dcb1df810bc78d8
claudiacasiano <p.casiano33@gmail.com>
2021-06-10 23:00:28 -0600

    add fail test

--- BackEnd/books_service/add_book/tests/addBook.spec.js
+++ BackEnd/books_service/add_book/tests/addBook.spec.js
@@ -5,77 +5,47 @@ const { assert } = require('chai');
 const { expect } = chai;
 
 
-describe('addProduct', () => {
+describe('addBook', () => {
     Server.set('port', 3001)
 
     it('Register a book', (done) => {
         request(Server)
-        .post('/add_product')
+        .post('/add_book')
         .send(
             {
                 titulo: "La metamorfosis",
                 autor: "Franz Kafka",
                 precio: 90.00,
                 cantidad: 2,
-                idEditorial: 31
+                id_editorial: 31
             }
         )
         .then(r => {
             expect(r.body)
                 .to.be.an.an('Object')
                 .that.has.property('message');
-            assert(r.body.code, 200);
+            assert(r.status, 200);
+            done(); 
         });
-        done(); 
     })
 
 
 
-    //     it('it should fail to set a product', async () => {
-    //         await request(Server)
-    //         .post('/add_product')
-    //         .send(
-    //             {
-    //                 name: 'productoprueba2',
-    //                 description: 'desc_prueba',
-    //                 price: 10.50,
-    //                 photo: 'prueba.png',
-    //                 owner_id: '6'
-                    
-    //             }
-    //         )
-    //         .then(r => {
-    //             console.log(r.body.code)
-               
-                
-    //             is_code_ret = r.body.code == 200
-                
-    //             assert.isTrue(is_code_ret)
-    //         });
-    // })
-
-//     it('it should fail to set an undefined product name', async () => {
-//         await request(Server)
-//         .post('/add_product')
-//         .send(
-//             {
-//                 name: undefined,
-//                 description: 'desc_prueba',
-//                 price: 10.50,
-//                 photo: 'prueba.png',
-//                 owner_id: 6
-                
-//             }
-//         )
-//         .then(r => {
-//             console.log(r.body.code)
-           
-            
-//             is_code_ret = r.body.code == 200
-            
-//             assert.isTrue(is_code_ret)
-//         });
-// })
-
-    
+    it('it should fail to set a product because id_editorial is missing', (done) => {
+            request(Server)
+            .post('/add_book')
+            .send(
+                {
+                    titulo: "La metamorfosis",
+                    autor: "Franz Kafka",
+                    precio: 90.00,
+                    cantidad: 2
+                }
+            )
+            .then(r => {
+                is_code_ret = r.status !== 200
+                assert.isTrue(is_code_ret)
+                done();
+            });
+    })    
 });
