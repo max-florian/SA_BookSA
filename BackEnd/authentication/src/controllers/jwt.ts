94ca57dae2a90917c5dfd838d5d0003e6bbd4a73
Darwin Arevalo <dalexis.da@gmail.com>
2021-06-13 22:44:07 -0600

    Verify token endpoint

--- BackEnd/authentication/src/controllers/jwt.ts
+++ BackEnd/authentication/src/controllers/jwt.ts
@@ -3,13 +3,13 @@ import { verify, sign } from "jsonwebtoken";
 import { setResponse } from "./response";
 import { JWT_SECRET, JWT_DURATION } from "../../env";
 
-export const verifyToken = (req: Request, res: Response, next: NextFunction) => {
+export const verifyToken = (req: Request, res: Response) => {
     const token = req.headers.authorization?.replace('Bearer ', '');
 
     if (token) {
         verify(token, JWT_SECRET, (err, _) => {
             if (err) return setResponse(res, { statuscode: 401, message: 'Token inválido', data: {} });
-            next();
+            setResponse(res, {statuscode: 200, message: 'Token correcto', data: {}});
         });
     } else {
         return setResponse(res, { statuscode: 400, message: 'Token no proveído.', data: {} });
--- BackEnd/authentication/src/routes/index.ts
+++ BackEnd/authentication/src/routes/index.ts
@@ -3,10 +3,12 @@ import {
     login,
     register
 } from "../controllers";
+import { verifyToken } from "../controllers/jwt";
 const router = Router();
 
 router.get('/', (req, res) => { res.send(`${req.app.get('servicename')} service working!`) })
 router.post('/login', login);
 router.post('/register', register);
+router.get('/verify-token', verifyToken);
 
 export default router;
