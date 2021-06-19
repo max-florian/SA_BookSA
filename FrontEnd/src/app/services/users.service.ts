4b4c9032c177dfe4ee667b8e2301d1530fa70def
Brayan Chinchilla <brayanch1297@gmail.com>
2021-06-19 00:53:13 -0600

    Communicates with backend service

--- FrontEnd/src/app/services/users.service.ts
+++ FrontEnd/src/app/services/users.service.ts
@@ -7,9 +7,20 @@ import { environment } from 'src/environments/environment';
 })
 export class UsersService {
 
+  userServer = environment.serverEditorial;
+  editorialServer = environment.serverEditorial;
+
   constructor(private http: HttpClient) { }
 
   getUsers(){
-    return this.http.get(`${environment.serverEditorial}/users`);
+    return this.http.get(`${this.userServer}/users`);
+  }
+
+  deleteUser(id_user:number){
+    return this.http.delete(`${this.userServer}/user/${id_user}`)
+  }
+
+  approveUser(id_editorial:number){
+    return this.http.post(`${this.editorialServer}/aprobar`, {id_editorial})
   }
 }
