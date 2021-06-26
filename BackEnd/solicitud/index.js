de4d5023ef9038eb721fc70a076432d2787f352b
ccastellanos <cristian6cas@hotmail.es>
2021-06-26 11:05:36 -0600

    Bugfix se elimina dependencias no necesarias del servico de solicitudes

--- BackEnd/solicitud/index.js
+++ BackEnd/solicitud/index.js
@@ -10,26 +10,6 @@ const mysql = require('./database');
 app.use(express.urlencoded());
 app.use(express.json());
 
-const uploadFile = (fileName) => {
-    // Read content from the file
-    const fileContent = fs.readFileSync(fileName);
-
-    // Setting up S3 upload parameters
-    const params = {
-        Bucket: BUCKET_NAME,
-        Key: 'cat.jpg', // File name you want to save as in S3
-        Body: fileContent
-    };
-
-    // Uploading files to the bucket
-    s3.upload(params, function(err, data) {
-        if (err) {
-            throw err;
-        }
-        console.log(`File uploaded successfully. ${data.Location}`);
-    });
-};
-
 app.post('/api/solicitud/add', async function (req, res) {
         let {titulo, autor, generos, url} = req.body;
         let code = 200;
